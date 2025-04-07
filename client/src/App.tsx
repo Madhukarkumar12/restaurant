cf79e9fca2af5a7f2c37833276f2e0592acb7a39
Madhukarkumar12 <madhukarkumar26072001@gmail.com>
2025-04-07 22:44:56 +0530

    restaurant

---
+++ client/src/App.tsx
@@ -0,0 +1,78 @@
+
+import './App.css'
+import Login from './auth/Login'
+import Signup from './auth/Signup'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import ForgotPassword from './auth/ForgotPassword'
+import ResetPassword from './auth/ResetPassword'
+import VerifyEmail from './auth/VerifyEmail'
+import HeroSection from './components/HeroSection'
+import MainLayout from './layout/MainLayout'
+import Profile from './components/Profile'
+import SearchPage from './components/SearchPage'
+import RestaurantDetail from './components/RestaurantDetail'
+import Cart from './components/Cart'
+
+const appRouter = createBrowserRouter([
+  {
+    path:"/",
+    element:<MainLayout/>,
+    children:[
+      {
+        path:"/",
+        element:<HeroSection/>
+      },
+      {
+        path:"/profile",
+        element:<Profile/>  
+      },
+      {
+        path:"/search/:text",
+        element:<SearchPage/>
+      },
+      {
+        path:"/restaurant/:id",
+        element:<RestaurantDetail/>
+      },
+      {
+        path:"/cart",
+        element:<Cart/>
+      }
+    ]
+  },
+  {
+    path:"/login",
+    element:<Login/>
+  },
+  {
+    path:"/signup",
+    element:<Signup/>
+  },
+  {
+    path:"/forgot-password",
+    element:<ForgotPassword/>
+  },
+  {
+    path:"/reset-password",
+    element:<ResetPassword/>
+  },
+  {
+    path:"/verify-email",
+    element:<VerifyEmail/>
+  }
+])
+
+function App() {
+
+  return (
+    <main>
+      <RouterProvider router={appRouter}>
+          
+      </RouterProvider>
+      
+    </main>
+  )
+}
+
+export default App
---
+++ client/src/components/CheckoutConfirmPage.tsx
@@ -0,0 +1,95 @@
+import { Dispatch, SetStateAction, useState } from "react"
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogTitle } from "./ui/dialog"
+import { Label } from "./ui/label"
+import { Input } from "./ui/input"
+import { Button } from "./ui/button"
+
+
+const CheckoutConfirmPage = ({ open, setOpen }: { open: boolean, setOpen: Dispatch<SetStateAction<boolean>> }) => {
+    const [input, setInput] = useState({
+        name: "",
+        email: "",
+        contact: "",
+        address: "",
+        city: "",
+        country: ""
+    })
+    const changeEventHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setInput({ ...input, [name]: value })
+    }
+    return (
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogContent>
+                <DialogTitle>Review Your Order</DialogTitle>
+                <DialogDescription className="text-xs">
+                    Review your order to ensure all items are correct before checkout. Confirm quantities, special instructions, and pricing for a smooth and satisfying dining experience.
+                </DialogDescription>
+                <form className="md:grid-cols-2 gap-2 space-y-1 md:space-0">
+                    <div >
+                        <Label>Full Name</Label>
+                        <Input
+                            type="text"
+                            name="fullname"
+                            value={input.name}
+                            onChange={changeEventHandler}
+                        />
+                    </div>
+                    <div >
+                        <Label>Email</Label>
+                        <Input
+                            type="email"
+                            name="email"
+                            value={input.email}
+                            onChange={changeEventHandler}
+                        />
+                    </div>
+                    <div >
+                        <Label>Contact</Label>
+                        <Input
+                            type="text"
+                            name="contact"
+                            value={input.contact}
+                            onChange={changeEventHandler}
+                        />
+                    </div>
+                    <div >
+                        <Label>address</Label>
+                        <Input
+                            type="text"
+                            name="address"
+                            value={input.address}
+                            onChange={changeEventHandler}
+                        />
+                    </div>
+                    <div >
+                        <Label>City</Label>
+                        <Input
+                            type="text"
+                            name="city"
+                            value={input.city}
+                            onChange={changeEventHandler}
+                        />
+                    </div>
+                    <div >
+                        <Label>Country</Label>
+                        <Input
+                            type="text"
+                            name="country"
+                            value={input.country}
+                            onChange={changeEventHandler}
+                        />
+                    </div>
+                    <DialogFooter className="col-span-2 pt-5 ">
+                        <Button className="bg-orange hover:bg-hoverOrange">Continue To Payment</Button>
+                    </DialogFooter>
+                </form>
+            </DialogContent>
+            {/* <DialogFooter>
+            <Button className="">Continue To Payment</Button>
+        </DialogFooter> */}
+        </Dialog>
+    )
+}
+
+export default CheckoutConfirmPage
